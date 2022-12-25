539c6e07942bda08de3350fa046118db57deb8ce
Artur <semenukartur1@gmail.com>
2022-12-26 01:06:29 +0200

    worc in React

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-);
-root.render(<React.StrictMode></React.StrictMode>);
+// without jsx
+// const title = React.createElement(
+//     'h1',
+//     {
+//         id: 'title',
+//     },
+//     'Hello React'
+// )
+
+// with jsx
+const title = <h1 id="title">Hello React</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(<React.StrictMode>{title}</React.StrictMode>)
