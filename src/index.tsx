270a5ddc860b7d0812229442f4c85b629e03405e
Artur <semenukartur1@gmail.com>
2022-12-26 01:33:35 +0200

    add React.Fradment

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const content = (
-    <div>
-        <ul>
-            <li>List item 1</li>
-            <li>List item 2</li>
-        </ul>
-        <p>Hello world</p>
-    </div>
-)
+const Title = () => {
+    return <h1>Hello React</h1>
+}
+
+const Content = () => {
+    return (
+        <React.Fragment>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Exercitationem ullam, perferendis et quaerat sequi totam saepe
+                molestias porro, dicta aspernatur, iusto ab adipisci facilis
+                dolorem sed tempora eligendi vero deleniti.
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Exercitationem ullam, perferendis et quaerat sequi totam saepe
+                molestias porro, dicta aspernatur, iusto ab adipisci facilis
+                dolorem sed tempora eligendi vero deleniti.
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Exercitationem ullam, perferendis et quaerat sequi totam saepe
+                molestias porro, dicta aspernatur, iusto ab adipisci facilis
+                dolorem sed tempora eligendi vero deleniti.
+            </p>
+        </React.Fragment>
+    )
+}
+function App() {
+    return (
+        <>
+            <Title />
+            <Content />
+        </>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
